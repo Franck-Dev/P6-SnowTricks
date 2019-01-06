788eed5637dcf10d6fbbd4a09491e88f98ec243b
Sorha <sorha.entropy@gmail.com>
2019-01-06 06:51:53 +0100

    Trick creation form, edit form + validation to make and authentification to pull

---
+++ src/Controller/TrickCreateController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Image;
+use App\Entity\Trick;
+use App\Form\TrickType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TrickCreateController extends AbstractController
+{
+    /**
+     * @Route("/trick/create", name="trick_create")
+     */
+    public function index(Request $request, ObjectManager $manager)
+    {
+        $trick = new Trick();
+
+        $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            foreach($trick->getImages() as $image)
+            {
+                $image->setTrick($trick);
+                $manager->persist($image);
+            }
+
+            $trick->setCreatedAt(new \DateTime());
+            $trick->setUpdatedAt(new \DateTime());
+            //$trick->setUser($user);
+
+            $manager->persist($trick);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Le trick <strong>' . $trick->getName() . '</strong> a bien été enregistré !'
+            );
+
+            return $this->redirectToRoute('trick_details', [
+                'slug' => $trick->getSlug()
+            ]);
+        }
+
+        return $this->render('trick_create/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
